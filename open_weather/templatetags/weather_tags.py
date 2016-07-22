3a2b5e995eddc46515cab3eef163848b09c8e87a
nishantp11 <nishant-pandey@hotmail.com>
2016-07-22 21:09:27 +0530

    readability

--- open_weather/templatetags/weather_tags.py
+++ open_weather/templatetags/weather_tags.py
@@ -1,7 +1,14 @@
+"""
+Templatetags for weather app
+"""
+
 from datetime import datetime
 from django import template    
 register = template.Library()    
 
 @register.filter('timestamp_to_time')
 def timestamp_to_time(in_string):
+	"""
+	Converts unix timestamp to python datetime object
+	"""
 	return datetime.fromtimestamp(int(in_string))
